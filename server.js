e9c26b3a96cb4f35d19f2127221db4baef061988
saxonmatt <saxonmatt@gmail.com>
2014-03-30 23:28:31 +0100

    Some more chat stuff (UI)... and message typing.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var socketServer = require("ws").Server,
 	http = require("http"),
 	express = require("express"),
 	app = express(),
+	guids = require("./guids"),
 	logger = require("./logger"),
 	port = process.env.PORT || 1337;
 
@@ -19,20 +20,52 @@ ws.on("connection", function(socket) {
 	setInterval(function() { ws.ping(socket); }, 30000);
 
 	// Echo messages...
-	socket.on("message", function(data) { ws.broadcast(JSON.parse(data)); });
+	socket.on("message", function(rawMessage) { 
+		var message = JSON.parse(rawMessage);
+		logger.info(message);
+		switch(message.type) {
+			case "chat":
+				ws.broadcast(message.user, message.data);
+				break;
+			case "join":
+				ws.register(socket, message.user);
+				break;
+		} 
+	});
 
 });
 
-ws.broadcast = function(msg) {
+ws.broadcast = function(user, msg) {
 	logger.info("Broadcasting user message...");
-	for(var i in this.clients)
-		this.clients[i].send(JSON.stringify(msg));
+	for(var i in this.clients) {
+		this.clients[i].send(JSON.stringify({
+			type: "chat",
+			data: {
+				name: user.name,
+				text: msg
+			}
+		}));
+	}
 };
 
 ws.ping = function(socket) {
 	logger.info("Ping!");
 	socket.send(JSON.stringify({
-		name: "Azure",
-		text: "ping..."
+		type: "chat",
+		data: {
+			name: "Azure",
+			text: "ping..."
+		}
+	}));
+};
+
+ws.register = function(socket, user) {
+	logger.info("Registering new user '" + user.name + "'.");
+	socket.send(JSON.stringify({
+		type: "joinResponse",
+		data: {
+			id: guids.guid(),
+			name: user.name
+		}
 	}));
 };
